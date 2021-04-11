8e59f0df3ab4c533b53f90b238894c23097fc5f5
Victor Lavalle <45529198+VictorLavalle@users.noreply.github.com>
2021-04-11 03:10:08 -0500

    Added the database and the tables apart in case of need them

---
+++ resources/database/feedrss.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-04-2021 a las 10:05:21
+-- Versión del servidor: 10.4.16-MariaDB
+-- Versión de PHP: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `feedrss`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `feedtable`
+--
+
+CREATE TABLE `feedtable` (
+  `idRSS` int(11) NOT NULL,
+  `RSSLink` varchar(100) NOT NULL,
+  `RSSTitle` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `noticias`
+--
+
+CREATE TABLE `noticias` (
+  `idnews` int(11) NOT NULL,
+  `idRSS` int(11) NOT NULL,
+  `fecha` varchar(100) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `enlace` varchar(400) NOT NULL,
+  `descripcion` varchar(400) DEFAULT NULL,
+  `cat` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `feedtable`
+--
+ALTER TABLE `feedtable`
+  ADD PRIMARY KEY (`RSSLink`),
+  ADD KEY `rss_id_index` (`idRSS`);
+
+--
+-- Indices de la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  ADD PRIMARY KEY (`enlace`),
+  ADD KEY `news_id_index` (`idnews`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `feedtable`
+--
+ALTER TABLE `feedtable`
+  MODIFY `idRSS` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  MODIFY `idnews` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
